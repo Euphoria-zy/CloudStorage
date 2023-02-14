5963deda51d7e7295b483946b44c8c7011317939
Simon <cuitianle01@126.com>
2023-02-14 17:30:15 +0800

    日志功能

--- CloudStorage-server/src/test/java/com/tcb/cloudstorage/CloudstorageApplicationTests.java
+++ CloudStorage-server/src/test/java/com/tcb/cloudstorage/CloudstorageApplicationTests.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.tcb.cloudstorage.domain.FileStore;
 import com.tcb.cloudstorage.domain.Folder;
+import com.tcb.cloudstorage.domain.UserLog;
 import com.tcb.cloudstorage.domain.User;
 import com.tcb.cloudstorage.mapper.FolderMapper;
 import com.tcb.cloudstorage.mapper.UserMapper;
 import com.tcb.cloudstorage.service.FileStoreService;
 import com.tcb.cloudstorage.utils.EmailService;
+import com.tcb.cloudstorage.service.LogService;
 import com.tcb.cloudstorage.utils.JedisUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,10 @@ import redis.clients.jedis.Jedis;
 
 import java.io.File;
 import java.security.GeneralSecurityException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @SpringBootTest
 class CloudstorageApplicationTests
@@ -25,9 +31,30 @@ class CloudstorageApplicationTests
     private UserMapper userMapper;
     @Autowired
     private FileStoreService fileStoreService;
-
+    @Autowired
+    private LogService logService;
     @Autowired
     private FolderMapper folderMapper;
+
+    @Test
+    void testRecordLog() {
+        logService.recordLog(UserLog.builder()
+                .userId(12)
+                .recordTime(new Timestamp(new Date().getTime()))
+                .operationType(2)
+                .isFile(true)
+                .fileFolderId(1)
+                .isOperationSuccess(true)
+                .build());
+    }
+    @Test
+    void testReadLog(){
+        List<UserLog> logs=logService.readLog(12);
+        for (UserLog log :
+                logs) {
+            System.out.println(log);
+        }
+    }
     @Test
     void sendEmail() throws GeneralSecurityException
     {
