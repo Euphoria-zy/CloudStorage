07c65f14db1ef0839d4c25c86037a217bcadb947
whoslcy <whoslcy@foxmail.com>
2023-02-14 11:51:52 +0800

    [front-end]: First commit.

---
+++ CloudStorage-view/src/components/TopBar.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+const userName = ref('User')
+</script>
+
+<template>
+    <header>
+        <div class="top-flex-container">
+            <div class="header-left">
+                <img src="../assets/product_logo.jpg" alt="产品logo" height="30">
+                <p>极速云盘</p>
+            </div>
+            <!-- 将网盘 logo 和用户信息隔开 -->
+            <div class="header-center"></div>
+            <div class="header-right">
+                <el-avatar></el-avatar>
+                <p>{{ userName }}</p>
+                <el-button link>退出登录</el-button>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    height: 56px;
+    box-shadow: 0 3px 10px 0 rgb(0 0 0 / 6%);
+    padding: 0 24px;
+}
+
+.top-flex-container, .header-left, .header-right  {
+    display: flex;
+    align-items: center;
+}
+
+.header-left {
+    justify-content: space-between;
+    min-width: 110px;
+}
+
+.header-right {
+    justify-content: space-between;
+    min-width: 156px;
+}
+.header-center {
+    flex: 1 100px;
+    min-width: 100px;
+}
+</style>
